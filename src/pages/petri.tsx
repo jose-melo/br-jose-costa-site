ac8a341a8c8cf5361d7199e2dd05d8c175a56ca7
jose-melo <jmcosta@usp.br>
2021-06-14 00:32:38 -0300

    feat(petri): add a petri net simulation for the producer consumer problem

---
+++ src/pages/petri.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import * as React from "react";
+import { PetriDetails } from "../modules/petri/svg-petri";
+
+const PetriPage: React.FC = () => <PetriDetails />;
+
+export default PetriPage;
